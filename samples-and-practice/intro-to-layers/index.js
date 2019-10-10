6ab7baeb231af82d0c522764fc5c09bd32f9a197
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-10 19:38:38 -0400

    FEAT(intro to layers): added tile layers

--- samples-and-practice/intro-to-layers/index.js
+++ samples-and-practice/intro-to-layers/index.js
@@ -1,4 +1,13 @@
-require(["esri/Map", "esri/views/SceneView"]), function (Map, SceneView) {
+require(["esri/Map", "esri/views/SceneView", "esri/layers/TileLayer"]), function (Map, SceneView, TileLayer) {
+
+  let transportationLayer = new TileLayer({
+    url: "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer"
+  })
+
+  let housingLayer = new TileLayer({
+    url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/New_York_Housing_Density/MapServer"
+  })
+
   let map = new Map({
     basemap: "oceans"
   })
