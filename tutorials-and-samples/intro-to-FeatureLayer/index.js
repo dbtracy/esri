1ef2c82b8edfd70c9c72155d16b882d075bb84d6
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-12 11:02:13 -0400

    FEAT(featurelayer): experiment with vector tile layer

--- tutorials-and-samples/intro-to-FeatureLayer/index.js
+++ tutorials-and-samples/intro-to-FeatureLayer/index.js
@@ -1,6 +1,6 @@
-require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (Map, MapView, FeatureLayer) {
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/layers/VectorTileLayer"], function (Map, MapView, FeatureLayer, VectorTileLayer) {
   let map = new Map({
-    basemap: "hybrid"
+    basemap: "hybrid",
   })
 
   let view = new MapView({
@@ -19,5 +19,15 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function
     url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0"
   })
 
+
+
+  let tileLayer = new VectorTileLayer({
+    url:
+      "https://jsapi.maps.arcgis.com/sharing/rest/content/items/75f4dfdff19e445395653121a95a85db/resources/styles/root.json"
+  })
+
+  // if featureLayer comes before tileLayer, the tile
+  // layer will not show up (it gets buried)
+  map.add(tileLayer)
   map.add(featureLayer)
 })
