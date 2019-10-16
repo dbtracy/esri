9d884587329cb53c8d0c67e90b5bd8360ea9f254
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-16 17:03:14 -0400

    added graphics layer

--- tutorials-and-samples/point-line-polygon-graphics/index.js
+++ tutorials-and-samples/point-line-polygon-graphics/index.js
@@ -1,17 +1,43 @@
 require([
   "esri/Map",
   "esri/views/MapView",
-], function (Map, MapView) {
+  "esri/Graphic",
+  "esri/layers/GraphicsLayer"
+], function (Map, MapView, Graphic, GraphicsLayer) {
 
-  var map = new Map({
+  let map = new Map({
     basemap: "topo-vector"
   });
 
-  var view = new MapView({
+  let view = new MapView({
     container: "viewDiv",
     map: map,
     center: [-118.80500, 34.02700],
     zoom: 13
   });
 
+  let graphicsLayer = new GraphicsLayer()
+  map.add(graphicsLayer)
+
+  let point = {
+    type: "point",
+    longitude: -118.80657463861,
+    latitude: 34.0005930608889
+  }
+
+  let simpleMarkerSymbol = {
+    type: "simple-marker",
+    color: [226, 119, 40],
+    outline: {
+      color: [255, 255, 255],
+      width: 1
+    }
+  }
+
+  let pointGraphic = new Graphic({
+    geometry: point,
+    symbol: simpleMarkerSymbol
+  })
+
+  graphicsLayer.add(pointGraphic)
 });
