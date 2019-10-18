e72caffa449d45264b2018ed96a18d55243908a5
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-18 15:30:29 -0400

    FEAT(minncidents): points rendering!

--- experiments/mn-police-incidents/index.js
+++ experiments/mn-police-incidents/index.js
@@ -1,8 +1,10 @@
 require([
   "dojo/request",
   "esri/Map",
-  "esri/views/MapView"
-], function (request, Map, MapView) {
+  "esri/views/MapView",
+  "esri/layers/GraphicsLayer",
+  "esri/Graphic"
+], function (request, Map, MapView, GraphicsLayer, Graphic) {
   const map = new Map({
     basemap: "streets"
   })
@@ -10,21 +12,52 @@ require([
   const view = new MapView({
     container: "viewDiv",
     map: map,
-    center: [-93.2, 44.9],
+    center: [-93.2650, 44.9778],
     zoom: 13
   })
 
+  let graphicsLayer = new GraphicsLayer()
+  map.add(graphicsLayer)
+
   const getData = async (req, res, next) => {
+    console.log(res)
     try {
-      let data = await request.get("https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Police_Incidents_2018_PIMS/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json", {
+      const data = await request.get("https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Police_Incidents_2018_PIMS/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json", {
         headers: {
           "X-Requested-With": null,
-          // "Access-Control-Allow-Origin": "*"
         }
       })
       if (data) {
-        data = JSON.parse(data)
-        console.log(data.features)
+        let incidentData = JSON.parse(data)
+        const incidents = incidentData.features.map(incident => {
+
+          // console.log(incidents)
+          let incidentLocation = {
+            type: "point",
+            longitude: incident.geometry.x,
+            latitude: incident.geometry.y,
+          }
+
+          let incidentMarker = {
+            type: "simple-marker",
+            color: [0, 150, 50],
+            outline: {
+              color: [255, 255, 255],
+              width: 2
+            },
+            size: 8
+          }
+
+          let incidentGraphic = new Graphic({
+            geometry: incidentLocation,
+            symbol: incidentMarker,
+          })
+
+          return incidentGraphic
+        })
+        console.log(incidentData)
+        console.log(incidents)
+        graphicsLayer.addMany(incidents)
       } else {
         console.log('STILL GOT PROBLEMS')
       }
@@ -32,6 +65,7 @@ require([
       console.log('Oh noes!!!', error)
     }
   }
-
   getData()
 })
+
+
