ea3e2646482b15a8d00d446db8511b32b931d738
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-18 15:39:10 -0400

    added bed-stuy polygon

--- experiments/ny-neighborhoods/index.js
+++ experiments/ny-neighborhoods/index.js
@@ -35,7 +35,44 @@ require([
     ]
   }
 
-  const fillSymbol = {
+  const crownHeightsFill = {
+    type: "simple-fill",
+    color: [50, 100, 255, .5],
+    outline: {
+      color: [50, 100, 255],
+      width: 1
+    },
+    style: "forward-diagonal"
+  }
+
+  let crownHeights = new Graphic({
+    geometry: crownHeightsPolygon,
+    symbol: crownHeightsFill,
+    attributes: {
+      Name: "Crown Heights"
+    },
+    popupTemplate: {
+      title: "{Name}"
+    }
+  })
+
+  const bedStuyPolygon = {
+    type: "polygon",
+    rings: [
+      [-73.958223, 40.679849], // atlantic and classon
+      [-73.961904, 40.697755],
+      [-73.961882, 40.698161],
+      [-73.957011, 40.698967],
+      [-73.941936, 40.700724],
+      [-73.918011, 40.687170],
+      [-73.916386, 40.678905],
+      [-73.916284, 40.678575],
+      [-73.916471, 40.676635],
+      [-73.952440, 40.678616]
+    ]
+  }
+
+  const bedStuyFill = {
     type: "simple-fill",
     color: [50, 100, 255, .5],
     outline: {
@@ -45,17 +82,18 @@ require([
     style: "backward-diagonal"
   }
 
-  let crownHeights = new Graphic({
-    geometry: crownHeightsPolygon,
-    symbol: fillSymbol,
+  const bedStuy = new Graphic({
+    geometry: bedStuyPolygon,
+    symbol: bedStuyFill,
     attributes: {
-      Name: "Crown Heights"
+      Name: "Bedford-Stuyvesant"
     },
     popupTemplate: {
-      title: "Crown Heights"
+      title: "{Name}"
     }
   })
 
   graphicsLayer.add(crownHeights)
+  graphicsLayer.add(bedStuy)
 })
 
