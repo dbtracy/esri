1b62bdbb46de8ff6baff8e112baa04b1227d2ba5
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-20 11:52:06 -0400

    FEAT(feature layer): added feature layer, MUCH simpler and complete rendering

--- experiments/mn-police-incidents/index.js
+++ experiments/mn-police-incidents/index.js
@@ -3,8 +3,10 @@ require([
   "esri/Map",
   "esri/views/MapView",
   "esri/layers/GraphicsLayer",
-  "esri/Graphic"
-], function (request, Map, MapView, GraphicsLayer, Graphic) {
+  "esri/layers/FeatureLayer",
+  "esri/Graphic",
+  "dojo/domReady!"
+], function (request, Map, MapView, GraphicsLayer, FeatureLayer, Graphic) {
   const map = new Map({
     basemap: "streets"
   })
@@ -19,69 +21,82 @@ require([
   let graphicsLayer = new GraphicsLayer()
   map.add(graphicsLayer)
 
-  const getData = async (req, res, next) => {
-    console.log(res)
-    try {
-      const data = await request.get("https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Police_Incidents_2018_PIMS/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json", {
-        headers: {
-          "X-Requested-With": null,
-        }
-      })
-      if (data) {
-        let incidentData = JSON.parse(data)
-        console.log(incidentData.features)
-        const incidents = incidentData.features.map(incident => {
+  // const getData = async (req, res, next) => {
+  //   console.log(res)
+  //   try {
+  //     const data = await request.get("https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Police_Incidents_2018_PIMS/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json", {
+  //       headers: {
+  //         "X-Requested-With": null,
+  //       }
+  //     })
+  //     if (data) {
+  //       let incidentData = JSON.parse(data)
+  //       console.log(incidentData.features)
+  //       const incidents = incidentData.features.map(incident => {
 
-          let incidentLocation = {
-            type: "point",
-            longitude: incident.geometry.x,
-            latitude: incident.geometry.y,
-          }
+  //         let incidentLocation = {
+  //           type: "point",
+  //           longitude: incident.geometry.x,
+  //           latitude: incident.geometry.y,
+  //         }
 
-          let incidentMarker = {
-            type: "simple-marker",
-            color: [0, 150, 50],
-            outline: {
-              color: [255, 255, 255],
-              width: 2
-            },
-            size: 8
-          }
+  //         let incidentMarker = {
+  //           type: "simple-marker",
+  //           color: [0, 150, 50],
+  //           outline: {
+  //             color: [255, 255, 255],
+  //             width: 2
+  //           },
+  //           size: 8
+  //         }
 
-          let incAtts = incident.attributes
+  //         let incAtts = incident.attributes
 
-          let attributes = {
-            Name: `${incAtts.offense}`,
-            Time: `${incAtts.reportedTime}`,
-            Address: `${incAtts.publicaddress}`,
-            Neighborhood: `${incAtts.neighborhood}`,
+  //         let attributes = {
+  //           Name: `${incAtts.offense}`,
+  //           Time: `${incAtts.reportedTime}`,
+  //           Address: `${incAtts.publicaddress}`,
+  //           Neighborhood: `${incAtts.neighborhood}`,
 
-          }
+  //         }
 
-          let popupTemplate = {
-            title: `{Name}`,
-            content: `<b>{Address}</b>` + "<br/>" +
-              `<b>{Neighborhood}, Minneapolis, MN</b>`
-          }
+  //         let popupTemplate = {
+  //           title: `{Name}`,
+  //           content: `<b>{Address}</b>` + "<br/>" +
+  //             `<b>{Neighborhood}, Minneapolis, MN</b>`
+  //         }
 
-          let incidentGraphic = new Graphic({
-            geometry: incidentLocation,
-            symbol: incidentMarker,
-            attributes: attributes,
-            popupTemplate: popupTemplate
-          })
+  //         let incidentGraphic = new Graphic({
+  //           geometry: incidentLocation,
+  //           symbol: incidentMarker,
+  //           attributes: attributes,
+  //           popupTemplate: popupTemplate
+  //         })
 
-          return incidentGraphic
-        })
-        graphicsLayer.addMany(incidents)
-      } else {
-        console.log('STILL GOT PROBLEMS')
-      }
-    } catch (error) {
-      console.log('Oh noes!!!', error)
-    }
-  }
-  getData()
+  //         return incidentGraphic
+  //       })
+  //       graphicsLayer.addMany(incidents)
+  //     } else {
+  //       console.log('STILL GOT PROBLEMS')
+  //     }
+  //   } catch (error) {
+  //     console.log('Oh noes!!!', error)
+  //   }
+  // }
+  // getData()
+
+  let serviceUrl = "https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Police_Incidents_2018_PIMS/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+
+  let layer = new FeatureLayer(serviceUrl, {
+    // featureReduction: {
+    //   type: "cluster"
+    // }
+  })
+  console.log(layer)
+  // layer.setFeatureReduction({
+  //   type: "cluster"
+  // })
+  map.layers.add(layer)
 })
 
 
